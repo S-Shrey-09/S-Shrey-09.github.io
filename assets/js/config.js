42f4072e68e97db35e7f8bb2aebabaf7c3338c15
S-Shrey-09 <swastikshrey@gmail.com>
2022-03-28 20:58:54 +0530

    28th March

--- assets/js/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    'share-selected-text': true,
-    'load-more': false,
-    'infinite-scroll': false,
-    'infinite-scroll-step': 1,
-    'disqus-shortname': 'hauntedthemes-demo',
-    'content-api-host': '',
-    'content-api-key': '',
-};
